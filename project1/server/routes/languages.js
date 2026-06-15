a52534a5e13e1656f63cb53b68548fba7be7f6ab
Aquila N. <aquilanuzhat@gmail.com>
2026-06-15 00:28:29 -0400

    project 2 turning static to database

--- project1/server/routes/languages.js
+++ project1/server/routes/languages.js
@@ -1,15 +1,13 @@
 import express from 'express'
 import path from 'path'
 import { fileURLToPath } from 'url'
-import languageData from '../data/languages.js'
+import LangController from '../controllers/languages.js'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
 const router = express.Router()
-router.get('/', (req, res) => {
-  res.status(200).json(languageData)
-})
+router.get('/', LangController.getLangs)
 
 router.get('/:languageId', (req, res) => {
   res.status(200).sendFile(path.resolve(__dirname, '../../front-end/public/language.html'))
--- project1/server/server.js
+++ project1/server/server.js
@@ -1,4 +1,5 @@
 import express from 'express'
+import dotenv from 'dotenv';
 import languagesRouter from './routes/languages.js'
 
 const app = express()
